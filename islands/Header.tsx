5d6476584d5c6d3a43ea0eda96c743665f73da73
n4ver <nicholaslkchng@gmail.com>
2022-11-23 23:13:00 +0800

    i dont know what i changed

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header>
             <div id="head" class="justify-center">
-                <div id="head-wrapper" class="flex-none h-full w-full flex-nowrap text-white">
+                <div id="head-wrapper" class="flex-none h-full w-full flex-nowrap">
                     <div id="nav-bar" class="flex items-center bg-gradient-to-r from-indigo-500 gap-4 h-16 px-1">
                         <div id="search-wrapper" class="flex px-4">
                             <form id="search-bar" class="flex">
@@ -12,17 +12,17 @@ export default function Header() {
                                             <path fill="white" d="M23.785 21.937L18.12 16.27a.554.554 0 0 0-.397-.163h-.616a9.683 9.683 0 0 0 2.362-6.348c0-5.363-4.346-9.709-9.71-9.709S.05 4.396.05 9.759s4.346 9.709 9.709 9.709a9.683 9.683 0 0 0 6.348-2.362v.616c0 .15.06.29.163.397l5.667 5.666c.22.22.574.22.794 0l1.054-1.054a.56.56 0 0 0 0-.794zM9.76 17.227c-4.126 0-7.468-3.342-7.468-7.468S5.633 2.29 9.759 2.29s7.468 3.342 7.468 7.468-3.342 7.468-7.468 7.468z"></path>
                                         </svg>
                                     </span>
-                                    <input type="text" id="search" style="background: none" required placeholder="Search..." class=" border-none outline-none text-black px-2 text-lg"></input>
+                                    <input type="text" id="search" style="background: none" required placeholder="Search..." class=" border-none outline-none text-white px-2 text-lg"></input>
                                 </div>
                             </form>
                         </div>
-                        <div id="nav-bar-inner" class="flex gap-4">
-                            <a href="/" title="Home" class="text-xl">Home</a>
-                            <a href="/about" title="About" class="text-xl">About</a>
-                            <a href="/stats" title="Tournament Statistics" class="text-xl">Stats</a>
+                        <div id="nav-bar-inner" class="flex-auto gap-6 text-white">
+                            <a href="/" title="Home" class="text-xl px-4">Home</a>
+                            <a href="/about" title="About" class="text-xl px-4">About</a>
+                            <a href="/stats" title="Tournament Statistics" class="text-xl px-4">Stats</a>
                         </div>
-                        <div id="login-bar" class="flex">
-                            <a href="/login" title="Log in" class="text-xl">Login</a>
+                        <div id="login-bar" class="flex pr-4">
+                            <a href="/login" title="Log in" class="text-xl text-indigo-900">Login</a>
                         </div>
                     </div>
                 </div>
