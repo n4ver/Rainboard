2b3535ece4044aef75d815a4340f444de4df8e13
n4ver <nicholaslkchng@gmail.com>
2022-11-23 16:58:56 +0800

    Convert header to component, Add 404

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,10 +1,19 @@
 // routes/about.tsx
+import { Header } from "../components/Header.tsx";
+import { asset } from "$fresh/runtime.ts";
+
 
 export default function AboutPage() {
     return (
-      <main>
-        <h1>About</h1>
-        <p>This is the about page.</p>
-      </main>
+      <>
+        <head>
+          <link rel="stylesheet" href={asset("style.css")}></link>
+        </head>
+        <main>
+          <Header />
+          <h1>About</h1>
+          <p>This is the about page.</p>
+        </main>
+      </>
     );
   }
