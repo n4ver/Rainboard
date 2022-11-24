7ef3504bc71c2ee684a59295d62b0c960d5c29d2
n4ver <nicholaslkchng@gmail.com>
2022-11-24 15:48:35 +0800

    Update Header

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
         <header>
             <div id="head" class="justify-center">
                 <div id="head-wrapper" class="flex-none h-full w-full flex-nowrap">
-                    <div id="nav-bar" class="flex items-center bg-gradient-to-r from-indigo-500 gap-4 h-16 px-1">
+                    <div id="nav-bar" class="flex items-center bg-gradient-to-r from-indigo-500 gap-4 h-16 px-1 uppercase">
                         <div id="search-wrapper" class="flex px-4">
                             <form id="search-bar" class="flex">
                                 <div class="flex flex-row flex-nowrap bg-indigo-900 rounded-full min-h-full pl-2">
@@ -17,12 +17,12 @@ export default function Header() {
                             </form>
                         </div>
                         <div id="nav-bar-inner" class="flex-auto gap-6 text-white">
-                            <a href="/" title="Home" class="text-xl px-4">Home</a>
-                            <a href="/about" title="About" class="text-xl px-4">About</a>
-                            <a href="/stats" title="Tournament Statistics" class="text-xl px-4">Stats</a>
+                            <a href="/" title="Home" class="text-xl px-4 tracking-wider">Home</a>
+                            <a href="/about" title="About" class="text-xl px-4 tracking-wider">About</a>
+                            <a href="/stats" title="Tournament Statistics" class="text-xl px-4 tracking-wider">Stats</a>
                         </div>
                         <div id="login-bar" class="flex pr-4">
-                            <a href="/login" title="Log in" class="text-xl text-indigo-900">Login</a>
+                            <a href="/login" title="Log in" class="text-xl text-indigo-900 tracking-wider">Login</a>
                         </div>
                     </div>
                 </div>
